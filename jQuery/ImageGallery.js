472b3025b73a939941ff8bc1075cf775eabad621
vhferrari <50025931+vhferrari@users.noreply.github.com>
2022-02-27 14:53:50 -0300

    Create ImageGallery.js

---
+++ jQuery/ImageGallery.js
@@ -0,0 +1,21 @@
+function setup() {
+    $('.remove').on('click', function () {
+        this.parentNode.parentNode.removeChild(this.parentNode);
+    });
+}
+
+// Example case. 
+document.body.innerHTML = `
+<div class="image">
+  <img src="https://bit.ly/3lmYVna" alt="First">
+  <button class="remove">X</button>
+</div>
+<div class="image">
+  <img src="https://bit.ly/3flyaMj" alt="Second">
+  <button class="remove">X</button>
+</div>`;
+
+setup();
+
+document.getElementsByClassName("remove")[0].click();
+console.log(document.body.innerHTML);
